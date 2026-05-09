0847e8489bfea9f78607a139d51994d81868f563
thachdv <thachdv.it.dev@gmail.com>
2026-05-10 00:36:51 +0700

    udpate

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { routeTree } from './routeTree.gen'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
 
 const queryClient = new QueryClient({
@@ -20,8 +20,8 @@ const router = createRouter({ routeTree })
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} /> 
-      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   </StrictMode>
 )
